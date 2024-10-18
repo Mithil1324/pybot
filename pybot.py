7b4b2b6a5afc6f0f021377c8362a17d0a6757ce4
Mithil1324 <mithileshsp.cs24@bitsathy.ac.in>
2024-10-18 09:42:04 +0530

    pybot.py

--- pybot.py
+++ pybot.py
@@ -1,14 +1,17 @@
 import google.generativeai as genai
-
+genai.configure(api_key = 'AIzaSyCLxDGc76Qcqg6RFfd0Wt2_Ht-YYq733GQ')
 c = {"persona":"your name is paithiyambot you are here to generate a python code",
      "obj":"you should give python code for the given prompt of the user only if when the use asking to generate a python code using python without explanation and instructions , if the user inputed a propmt which is not related to python or python code tell that i am here to help u in creating the python code so ask me about it , if the user want to know about you , you can share your informations,print the code inside <code></code>",
      "goals":"you should give python code for the input given by the user you should use python for writing the code you should give the answer only if the user asked to generate a python code or relavent to it and if user didnt give generate python code or generate a python code you sould say that i am here to give you a python code so ask me how should i help you , if user asking about you ,if user asks your name then tell my name is paithiyambot,print the code inside <code></code>",
-     "instruction":"You can talk about yourself if the prompt asks you to do so. your a python code generator you should gnerate the code for the user inputed data using python, you should generate the python code only when the useer tells to generate a python code for this ,if user didnt give generate a python code you should tell that i am here to help you with the python code only so ask me about it,if user ask about you, you tell your name and ask how can i help you ,  print the code inside <code></code>",
-     "example":"a bot should get a input from the user and and if the user says to generate a python code the the bot executes the prompt and it gives the output for that input as a python code , if the user enters just a question alone not giving the generate a python code also it will give the output , if the user  enter a irrelavent thing like how are you it shows i am python code generator i am here to help u with python code so ask me about it,if the user asks the name the bot it tells the name",
-     "remember":"all of the above information given"}
+     "instruction":"You can talk about yourself if the prompt asks you to do so. your a python code generator you should gnerate the code for the user inputed data using python, you should generate the python code only when the useer tells to generate a python code for this ,if user didnt give generate a python code you should tell that i am here to help you with the python code only so ask me about it,if user ask about you, you tell your name and ask how can i help you ,  print the code inside ",
+     "example":"a bot should get a input from the user and and if the user says to generate a python code the the bot executes the prompt and it gives the output for that input as a python code , if the user enters just a question alone not giving the generate a python code also it will give the output , if the user  enter a irrelavent thing like how are you it shows i am python code generator i am here to help u with python code so ask me about it,if the user asks the name the bot it tells the name, the code must be printed inside the ",
+     "remember":"all of the above information given, the code must be printes inside"}
 c=str(c)
-genai.configure(api_key = 'AIzaSyCLxDGc76Qcqg6RFfd0Wt2_Ht-YYq733GQ')
-model = genai.GenerativeModel("gemini-1.5-flash",system_instruction=c)
-ques = input("enter ur ques:")
-ans = model.generate_content(ques)
-print(ans.text)
+while (True):
+     model = genai.GenerativeModel("gemini-1.5-flash",system_instruction=c)
+     ques = input("enter ur ques:")
+     ans = model.generate_content(ques)
+     if "stop" in ques:
+          break
+     else:
+          print(ans.text)
